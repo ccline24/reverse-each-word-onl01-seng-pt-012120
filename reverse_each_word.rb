e8b336e3143ee287bc8f27cf09b41a09239d0442
Chance cline <clinechance@gmail.com>
2020-01-30 21:27:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(sentence)
     sentence.split
     new_sentance = sentence.split 
     new_sentance.map do |word|
-     reverse_sentance = map
+     reverse_sentance = word
 
 end
