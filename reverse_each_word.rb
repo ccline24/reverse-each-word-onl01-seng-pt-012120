29d6af00ec6e66042e6b9bb7859d884691a429c0
Chance cline <clinechance@gmail.com>
2020-01-30 21:56:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
-
-def reverse_each_word(sentence)
-    sentence.split
-    new_sentance = sentence.split 
-    reverse_sentance = new_sentance.map do |word|
-      word.reverse.join(" ")
-    end
-
+def reverse_each_word(array)
+  rev_words = []
+  new_array = "#{array}".split(" ")
+  new_array.collect do |word|
+    rev_words << word.reverse
+  end
+  rev_words.join(" ")
 end
